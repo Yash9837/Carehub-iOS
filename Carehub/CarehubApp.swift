4c9bc61ede6ca5662846ad701932c13fa554a6af
Anurag Dutta <126210289+Anurag12kaari@users.noreply.github.com>
2025-04-15 19:24:48 +0530

    Initial Commit

---
+++ Carehub/CarehubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarehubApp.swift
+//  Carehub
+//
+//  Created by Anurag on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CarehubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
