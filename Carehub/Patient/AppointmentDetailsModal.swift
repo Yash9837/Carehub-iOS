e6d3962877d8e6739e0c8045384111b5b9fbaa26
Anurag Dutta <126210289+Anurag12kaari@users.noreply.github.com>
2025-04-28 00:37:15 +0530

    add:Multiple files

---
+++ Carehub/Patient/AppointmentDetailsModal.swift
@@ -0,0 +1,18 @@
+//
+//  AppointmentDetailsModal.swift
+//  Carehub
+//
+//  Created by Anurag on 27/04/25.
+//
+
+import SwiftUI
+
+struct AppointmentDetailsModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppointmentDetailsModal()
+}
